ee5e40243263f5d1373c1127f2d095f266bd7ef9
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2023-06-15 00:05:21 -0400

    agregado genre crud con exito

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtistController;
 use App\Http\Controllers\SongController;
+use App\Http\Controllers\GenresController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,19 +20,29 @@ use App\Http\Controllers\SongController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 //artist
 Route::get('/artists', [ArtistController::class, 'index']);
-Route::post('/artists', [ArtistController::class, 'store']);
-Route::get('/artists/{id}', [ArtistController::class, 'show']);
-Route::delete('/artists/{id}', [ArtistController::class, 'destroy']);
-Route::put('/artists/{id}', [ArtistController::class, 'update']);
+Route::post('/artists/post', [ArtistController::class, 'store']);
+Route::get('/artists/get/{id}', [ArtistController::class, 'show']);
+Route::delete('/artists/delete/{id}', [ArtistController::class, 'destroy']);
+Route::put('/artists/put/{id}', [ArtistController::class, 'update']);
 
 //songs
 Route::get('/songs', [SongController::class, 'index']);
-Route::post('/songs', [SongController::class, 'store']);
-Route::get('/songs/file/{id}', [SongController::class, 'showFile']);
-Route::get('/songs/name/{id}', [SongController::class, 'showName']);
+Route::post('/songs/post', [SongController::class, 'store']);
+Route::get('/songs/get/file/{id}', [SongController::class, 'showFile']);
+Route::get('/songs/get/name/{id}', [SongController::class, 'showName']);
 
-Route::delete('/songs/{id}', [SongController::class, 'destroy']);
-Route::put('/songs/{id}', [SongController::class, 'update']);
+Route::delete('/songs/delete/{id}', [SongController::class, 'destroy']);
+Route::put('/songs/put/{id}', [SongController::class, 'update']);
 
+//genre
+Route::get('/genre', [GenresController::class, 'index']);
+Route::post('/genre/post', [GenresController::class, 'store']);
+Route::get('/genre/get/{id}', [GenresController::class, 'show']);
+Route::delete('/genre/delete/{id}', [GenresController::class, 'destroy']);
+Route::put('/genre/put/{id}', [GenresController::class, 'update']);
+
+
+//

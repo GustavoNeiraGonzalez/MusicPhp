da31bb5ccb411f287abe14b377f7a7c6fb801437
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2023-07-12 00:33:12 -0400

    api.php he cambiado rutas para que put delete y post se necesite ser admin si se requiere

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -25,52 +25,54 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //donde api es parte si o si del link http://127.0.0.1:8000/api/
 //artist
 Route::get('/artists', [ArtistController::class, 'index']);
-Route::post('/artists/post', [ArtistController::class, 'store']);
 Route::get('/artists/get/{id}', [ArtistController::class, 'show']);
-Route::delete('/artists/delete/{id}', [ArtistController::class, 'destroy']);
-Route::put('/artists/put/{id}', [ArtistController::class, 'update']);
 
 //songs
 Route::get('/songs', [SongController::class, 'index']);
-Route::post('/songs/post', [SongController::class, 'store']);
 Route::get('/songs/get/file/{id}', [SongController::class, 'showFile']);
 Route::get('/songs/get/name/{id}', [SongController::class, 'showName']);
 
-Route::delete('/songs/delete/{id}', [SongController::class, 'destroy']);
-Route::put('/songs/put/{id}', [SongController::class, 'update']);
-
 //genre
 Route::get('/genre', [GenresController::class, 'index']);
-Route::post('/genre/post', [GenresController::class, 'store']);
 Route::get('/genre/get/{id}', [GenresController::class, 'show']);
-Route::delete('/genre/delete/{id}', [GenresController::class, 'destroy']);
-Route::put('/genre/put/{id}', [GenresController::class, 'update']);
 
-
-// unir tablas
-//artista cancion
-Route::post('/atach/artists/songs', [ArtistController::class, 'attachsong']);
-Route::post('/detach/artists/songs', [ArtistController::class, 'attachgenre']);
-
-//artista genero
-Route::post('/atach/artists/genres', [ArtistController::class, 'attachgenre']);
-Route::post('/detach/artists/genres', [ArtistController::class, 'detachgenre']);
-
-//cancion genero
-Route::post('/atach/songs/genres', [SongController::class, 'attachgenre']);
-Route::post('/detach/songs/genres', [SongController::class, 'detachgenre']);
+// index users
 Route::get('/users', [UserController::class, 'index'])->name('index');;
 // -------------------- rutas para admins ----------------
 Route::group([
     'middleware' => 'role:admin'
     ], function ($router) {
-        /* roles */
-        Route::post('/delete/users/roles', [UserController::class, 'removeRole']);
-        Route::post('/put/users/roles', [UserController::class, 'assignRole']);
+        //artist
+        Route::post('/artists/post', [ArtistController::class, 'store']);
+        Route::delete('/artists/delete/{id}', [ArtistController::class, 'destroy']);
+        Route::put('/artists/put/{id}', [ArtistController::class, 'update']);
 
+        //songs
+        Route::post('/songs/post', [SongController::class, 'store']);
+        Route::delete('/songs/delete/{id}', [SongController::class, 'destroy']);
+        Route::put('/songs/put/{id}', [SongController::class, 'update']);
+
+        //genre
+        Route::post('/genre/post', [GenresController::class, 'store']);
+        Route::delete('/genre/delete/{id}', [GenresController::class, 'destroy']);
+        Route::put('/genre/put/{id}', [GenresController::class, 'update']);
+
+        //unir tablas artista genero
+        Route::post('/atach/artists/genres', [ArtistController::class, 'attachgenre']);
+        Route::post('/detach/artists/genres', [ArtistController::class, 'detachgenre']);
+
+        /* unir tablas cancion genero */
+        
+        Route::post('/atach/songs/genres', [SongController::class, 'attachgenre']);
+        Route::post('/detach/songs/genres', [SongController::class, 'detachgenre']);
+        
         /*unir tablas user song */
         Route::post('atach/users/songs', [UserController::class, 'attachsong']);
         Route::post('detach/users/songs', [UserController::class, 'detachsong']);
+
+        /* roles */
+        Route::post('/delete/users/roles', [UserController::class, 'removeRole']);
+        Route::post('/put/users/roles', [UserController::class, 'assignRole']);
         
         /*delete update users */
         Route::delete('/users/delete/{id}', [UserController::class, 'destroy']);
